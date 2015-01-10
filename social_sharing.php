e9762e655debf4db8b90a5c8ee35e9a5520cfca1
root <root@ubuntu.(none)>
2015-01-09 21:55:09 -0500

    Initial Commit

---
+++ social_sharing.php
@@ -0,0 +1,7 @@
+
+<a href="https://api.addthis.com/oexchange/0.8/forward/facebook/offer?url=<?php echo($post_link);?>&pubid=ra-54ac028516143ef0&ct=1&title=<?php echo($post_title); ?>&pco=tbxnj-1.0" target="_blank"><img src="https://cache.addthiscdn.com/icons/v2/thumbs/32x32/facebook.png" border="0" alt="Facebook"/></a>
+<a href="https://api.addthis.com/oexchange/0.8/forward/twitter/offer?url=<?php echo($post_link);?>&pubid=ra-54ac028516143ef0&ct=1&title=<?php echo($post_title); ?>&pco=tbxnj-1.0" target="_blank"><img src="https://cache.addthiscdn.com/icons/v2/thumbs/32x32/twitter.png" border="0" alt="Twitter"/></a>
+<a href="https://api.addthis.com/oexchange/0.8/forward/google_plusone_share/offer?url=<?php echo($post_link);?>&pubid=ra-54ac028516143ef0&ct=1&title=<?php echo($post_title); ?>&pco=tbxnj-1.0" target="_blank"><img src="https://cache.addthiscdn.com/icons/v2/thumbs/32x32/google_plusone_share.png" border="0" alt="Google+"/></a>
+<a href="https://api.addthis.com/oexchange/0.8/forward/linkedin/offer?url=<?php echo($post_link);?>&pubid=ra-54ac028516143ef0&ct=1&title=<?php echo($post_title); ?>&pco=tbxnj-1.0" target="_blank"><img src="https://cache.addthiscdn.com/icons/v2/thumbs/32x32/linkedin.png" border="0" alt="LinkedIn"/></a>
+
+
